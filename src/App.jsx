56af599cddfe5a64e2dd43b4c82dfbac0751de1d
Banti Singh <testingvisitor896@gmail.com>
2025-06-20 17:40:06 +0530

    routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react' 
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
  
 
 function App() {
  
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Navigate to="/login"></Navigate>} />
+        <Route path="/login" element={<div>login compoennet</div>} />
+        <Route path="/sing-in" element={<div>signin compoennet</div>} />
+        <Route path="*" element={<div>not found</div>} />
+      </Routes>
     </>
   )
 }
